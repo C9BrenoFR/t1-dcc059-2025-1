828c232f2dfc9e4937e87d9dad203862b700505f
C9BrenoFR <brenufr@gmail.com>
2025-07-02 01:04:45 -0300

    Adiciona origem ao nó

--- src/Aresta.cpp
+++ src/Aresta.cpp
@@ -1,9 +1,15 @@
 #include "Aresta.h"
 
-Aresta::Aresta(char id_no_alvo, int peso)
+Aresta::Aresta(char id_no_origem, char id_no_alvo, int peso)
 {
+    this->id_no_origem = id_no_origem;
     this->id_no_alvo = id_no_alvo;
     this->peso = peso;
+}
+
+char Aresta::getIdNoOrigem()
+{
+    return id_no_origem;
 }
 
 char Aresta::getIdNoAlvo()
@@ -14,6 +20,11 @@ char Aresta::getIdNoAlvo()
 int Aresta::getPeso()
 {
     return peso;
+}
+
+void Aresta::setIdNoOrigem(char id_no_origem)
+{
+    this->id_no_origem = id_no_origem;
 }
 
 void Aresta::setIdNoAlvo(char id_no_alvo)
--- src/Aresta.h
+++ src/Aresta.h
@@ -5,17 +5,20 @@ using namespace std;
 class Aresta
 {
 private:
+    char id_no_origem;
     char id_no_alvo;
     int peso;
 
 public:
-    Aresta(char id_no_alvo, int peso);
+    Aresta(char id_no_origem, char id_no_alvo, int peso);
     ~Aresta();
 
     char getIdNoAlvo();
+    char getIdNoOrigem();
     int getPeso();
 
     void setIdNoAlvo(char id_no_alvo);
+    void setIdNoOrigem(char id_no_origem);
     void setPeso(int peso);
 };
 
